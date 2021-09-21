4d0ddbb3af725a65ab1ef71dcb8cf6714dc5afd0
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-09-21 13:54:02 -0700

    Implement Cacheable on the game proxy

--- pkg/gateway/gameproxy.go
+++ pkg/gateway/gameproxy.go
@@ -1,18 +1,69 @@
 package gateway
 
 import (
+	"encoding/json"
 	"log"
 	"net"
 
+	"github.com/karashiiro/kartlobby/pkg/caching"
 	"github.com/karashiiro/kartlobby/pkg/gameinstance"
 	"github.com/karashiiro/kartlobby/pkg/network"
 )
 
 type gameProxy struct {
+	playerAddr   net.Addr
 	playerConn   network.Connection
+	gameAddr     net.Addr
 	gameConn     network.Connection
 	proxy        *net.UDPConn
+	proxyPort    int
 	proxyRunning bool
+}
+
+// GameProxyCached represents the data needed to reconstruct a game proxy
+// from serialized data.
+type GameProxyCached struct {
+	PlayerAddr *caching.CachedAddr
+	GameAddr   *caching.CachedAddr
+	ProxyPort  int
+}
+
+func (p *gameProxy) SerializeSelf() ([]byte, error) {
+	o := GameProxyCached{
+		PlayerAddr: caching.NewAddr(p.playerConn.Addr()),
+		GameAddr:   caching.NewAddr(p.gameConn.Addr()),
+		ProxyPort:  p.proxyPort,
+	}
+	return json.Marshal(&o)
+}
+
+func (p *gameProxy) DeserializeSelf(data []byte) error {
+	o := GameProxyCached{}
+
+	err := json.Unmarshal(data, &o)
+	if err != nil {
+		return err
+	}
+
+	p.playerAddr = o.PlayerAddr
+	p.gameAddr = o.GameAddr
+	p.proxyPort = o.ProxyPort
+
+	return nil
+}
+
+func (p *gameProxy) HydrateDeserialized(gatewayServer *net.UDPConn) error {
+	// Restart the UDP server on our proxy port
+	proxy, err := net.ListenUDP("udp", &net.UDPAddr{Port: p.proxyPort})
+	if err != nil {
+		return err
+	}
+
+	p.playerConn = network.NewUDPConnection(gatewayServer, p.playerAddr)
+	p.gameConn = network.NewUDPConnection(proxy, p.gameAddr)
+	p.proxy = proxy
+
+	return nil
 }
 
 func newGameProxy(playerConn network.Connection, inst *gameinstance.GameInstance) (*gameProxy, error) {
@@ -29,11 +80,14 @@ func newGameProxy(playerConn network.Connection, inst *gameinstance.GameInstance
 	}
 
 	return &gameProxy{
+		playerAddr: playerConn.Addr(),
 		playerConn: playerConn,
 		// Set the remote connection to one that directs messages *from* the
 		// proxy server *to* the game
-		gameConn: network.NewUDPConnection(proxy, inst.Conn.Addr()),
-		proxy:    proxy,
+		gameAddr:  inst.Conn.Addr(),
+		gameConn:  network.NewUDPConnection(proxy, inst.Conn.Addr()),
+		proxy:     proxy,
+		proxyPort: proxyPort,
 	}, nil
 }
 
